a947f99f7c96d004e2ce2f83144ef5bc8026f402
Franchette Camoro <nicolecamoro@gmail.com>
2025-08-23 20:10:04 +0800

    feat: disable sign in with google

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -91,19 +91,19 @@ export function LoginForm({
         <Button type="submit" className="w-full">
           Login
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+        {/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
             Or continue with
           </span>
-        </div>
-        <Button
+        </div> */}
+        {/* {       <Button
           variant="outline"
           className="w-full"
           onClick={handleGoogleLogin}
           disabled={submitting}
         >
           Login with Google
-        </Button>
+        </Button>} */}
       </div>
       {error ? <p className="mt-2 text-sm text-red-600">{error}</p> : null}
       <div className="text-center text-sm">
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -87,7 +87,7 @@ export function SignupForm({
         <Button type="submit" className="w-full" disabled={submitting}>
           Sign up
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+        {/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
           <span className="bg-background text-muted-foreground relative z-10 px-2">
             Or continue with
           </span>
@@ -99,7 +99,7 @@ export function SignupForm({
           disabled={submitting}
         >
           Continue with Google
-        </Button>
+        </Button> */}
       </div>
       {error ? <p className="mt-2 text-sm text-red-600">{error}</p> : null}
       <div className="text-center text-sm">
